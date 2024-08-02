6b80ba97d91796fa63bb6755bed9fcfd30b23a90
mohammed-mlh <mallahimohammed1@gmail.com>
2024-08-02 19:31:01 +0100

    f

--- cms/urls.py
+++ cms/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path("", include("core.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 handler404 = 'core.views.custom_404_view'
 handler500 = 'core.views.custom_500_view'
